e5241029956bae0440aa754ab05417e6b0e26472
Oscar Keur <o.e.keur@gmail.com>
2019-04-15 17:29:03 +0200

    Move files into new structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,6 @@
 const gulp = require('gulp');
 const stylelint = require('gulp-stylelint');
 const eslint = require('gulp-eslint');
-const js = require('gulp-uglify-es').default;
-const concat = require('gulp-concat');
 const scss = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleancss = require('gulp-clean-css');
@@ -12,27 +10,39 @@ const sourcemaps = require('gulp-sourcemaps');
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
 const livereload = require('gulp-livereload');
+const del = require('del');
+const babel = require('gulp-babel');
+const minify = require('gulp-uglify');
 
-const path_js = 'assets/js/src/**/*.js';
-const path_scss = 'assets/scss/**/*.scss';
-const path_style = 'assets/scss/style.scss';
-const path_dest = 'assets/';
+const path_js = 'assets/js/src/*.js';
+const path_scss = 'assets/css/scss/*.scss';
+const path_dest_css = 'assets/css';
+const path_dest_js = 'assets/js';
 
 
 let error_handler = {
-	errorHandler: notify.onError({
-		title: 'Gulp',
-		message: 'Error: <%= error.message %>'
-	})
+  errorHandler: notify.onError({
+    title: 'Gulp',
+    message: 'Error: <%= error.message %>'
+  })
 };
 
 function lint_css() {
-	return gulp.src(path_scss)
-		.pipe(plumber(error_handler))
-		.pipe(stylelint({
-			reporters: [{ formatter: 'string', console: true}]
-		}))
-		.pipe(livereload());
+  return gulp.src(path_scss)
+    .pipe(plumber(error_handler))
+    .pipe(stylelint({
+      reporters: [{ formatter: 'string', console: true}]
+    }))
+    .pipe(livereload());
+}
+
+function lint_js() {
+  return gulp.src(path_js)
+    .pipe(plumber(error_handler))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(livereload());
 }
 
 function fix_css() {
@@ -42,54 +52,71 @@ function fix_css() {
       reporters: [{ formatter: 'string', console: true}],
       fix: true
     }))
-    .pipe(gulp.dest('assets/scss'))
+    .pipe(gulp.dest('assets/css/scss/'));
 //  This outputs to 'assets/scss/**/*.scss', manual copy to right directory for now..
 }
 
-function lint_js() {
-	return gulp.src(path_js)
-		.pipe(plumber(error_handler))
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError())
-		.pipe(livereload());
+function fix_js() {
+  return gulp.src(path_js)
+    .pipe(plumber(error_handler))
+    .pipe(eslint({fix:true}))
+    .pipe(eslint.format())
+    .pipe(gulp.dest('assets/js/src'));
 }
 
 // TODO configure gulp-sass-glob to auto include all .scss files
 function sass() {
-	return gulp.src(path_style)
-		.pipe(plumber(error_handler))
-		.pipe(sourcemaps.init())
-		.pipe(scss().on('error', scss.logError))
+  clean_css_maps;
+  return gulp.src(path_scss)
+    .pipe(plumber(error_handler))
+    .pipe(sourcemaps.init())
+    .pipe(scss().on('error', scss.logError))
     .pipe(autoprefixer())
-		.pipe(cleancss({rebase: false, level :2}))
-		.pipe(sourcemaps.write('/maps/'))
-		.pipe(gulp.dest(path_dest + '/css'))
-		.pipe(livereload());
+    .pipe(cleancss({rebase: false, level :2}))
+    .pipe(sourcemaps.write('/maps/'))
+    .pipe(gulp.dest(path_dest_css))
+    .pipe(livereload());
 }
 
 function uglify() {
-	return gulp.src(path_js)
-		.pipe(plumber(error_handler))
-		.pipe(sourcemaps.init())
-		.pipe(concat('main.js'))
-		.pipe(js())
-		.pipe(sourcemaps.write('/maps/'))
-		.pipe(gulp.dest(path_dest + '/js'))
-		.pipe(livereload());
+  gulp.parallel(clean_js, clean_js_maps);
+  return gulp.src(path_js)
+    .pipe(plumber(error_handler))
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(minify())
+    .pipe(sourcemaps.write('/maps/'))
+    .pipe(gulp.dest(path_dest_js))
+    .pipe(livereload());
+}
+
+function clean_css_maps () {
+  return del(path_dest_css+'/maps/*');
+}
+
+function clean_js_maps () {
+  return del(path_dest_js+'/maps/*');
+}
+
+function clean_js () {
+  return del([path_dest_js+'/*.js', '!'+path_dest_js+'/vue*', !path_dest_js+'/src' ]);
 }
 
 function watch() {
-	livereload.listen({'port': 35729});
-	gulp.watch(path_scss, gulp.series(lint_css, sass));
-	// gulp.watch(path_js, gulp.series(lint_js, uglify));
+  livereload.listen({'port': 35730});
+  gulp.watch(path_scss, gulp.series(lint_css, sass));
+  gulp.watch(path_js, gulp.series(lint_js, uglify));
 }
 
-exports.fix = fix_css;
+exports.fix =  gulp.parallel(fix_css, fix_js);
 exports.sass = sass;
+exports.clean_all= gulp.parallel(clean_js, clean_css_maps, clean_js_maps);
+exports.clean_css= clean_css_maps
+exports.clean_js= gulp.parallel(clean_js, clean_js_maps);
 exports.uglify = uglify;
 exports.watch = watch;
-exports.test = gulp.parallel(lint_css);
-// exports.test = gulp.parallel(lint_css, lint_js);
-// exports.default = gulp.series(lint_css, lint_js, sass, uglify);
-exports.default = gulp.series(lint_css, sass);
+exports.test = gulp.parallel(lint_css, lint_js);
+exports.default = gulp.series(lint_css, lint_js, sass, uglify);
+
