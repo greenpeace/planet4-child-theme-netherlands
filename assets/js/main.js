8757eed67deed4953fd8ae2321657583bbd9ac6a
Oscar Keur <o.e.keur@gmail.com>
2019-05-09 17:06:00 +0200

    Add some console.logs to try and find a new developer :)

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+console.log('%c Hee! Psst! ', 'color: #6c0');
+console.log('%c Op zoek naar een baan? ', 'color: #6c0');
+console.log('%c We hebben een vacature voor een fullstack webdeveloper / support engineer ', 'color: #6c0');
+console.log('%c Reageer voor 10 juni ', 'color: #6c0');
+console.log('%c Zie onderstaande link: ', 'color: #6c0');
+console.log('%c www.greenpeace.nl/webdev ', 'color: #6c0');
