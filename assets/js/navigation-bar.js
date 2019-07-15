17d6b75e72a9b5d090d1ef627615db48b0232714
dirk <d.j.faber@outlook.com>
2019-07-15 14:42:03 +0200

    Show IE-10 error (fix)

--- assets/js/navigation-bar.js
+++ assets/js/navigation-bar.js
@@ -1,2 +1,2 @@
-"use strict";function isIE(){var i=window.navigator.userAgent,n=i.indexOf("MSIE "),t=i.indexOf("Trident/");return 0<n||0<t}function ShowIEAlert(){isIE()&&$(".old-ie-browser-notification").show()}
+"use strict";-1!==navigator.appVersion.indexOf("MSIE 10")&&$(".old-ie-browser-notification").show();
 //# sourceMappingURL=maps/navigation-bar.js.map
--- assets/js/src/navigation-bar.js
+++ assets/js/src/navigation-bar.js
@@ -1,16 +1,6 @@
 // Display an error message when user uses IE10 or below.
-function isIE() {
-  var ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
-  var msie = ua.indexOf('MSIE '); // IE 10 or older
-  var trident = ua.indexOf('Trident/'); //IE 11
-
-  return (msie > 0 || trident > 0);
-}
-
-
-//function to show alert if it's IE
-function ShowIEAlert(){
-  if(isIE()){
-    $(".old-ie-browser-notification").show();
-  }
+// IF THE BROWSER IS INTERNET EXPLORER 10
+if (navigator.appVersion.indexOf("MSIE 10") !== -1)
+{
+  $(".old-ie-browser-notification").show();
 }
