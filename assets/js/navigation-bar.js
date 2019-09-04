1f40565e8bab0d4943e91a9b39739eaa4e2f602f
Dirk Faber <dirk.faber@greenpeace.org>
2019-09-04 12:25:24 +0200

    fix donate button bug on non-touch devices
    Clicking the button on non-touch devices should forward the user to another page and not open the dropdown menu.

--- assets/js/navigation-bar.js
+++ assets/js/navigation-bar.js
@@ -1,2 +1,2 @@
-"use strict";0<=navigator.userAgent.indexOf("MSIE")&&jQuery(".old-ie-browser-notification").show(),jQuery(document).ready(function(){var e=jQuery(".btn-donate");"ontouchstart"in window==!0&&e.removeAttr("href")});
+"use strict";0<=navigator.userAgent.indexOf("MSIE")&&jQuery(".old-ie-browser-notification").show(),jQuery(document).ready(function(){var t=jQuery(".btn-donate");"ontouchstart"in window==!0&&(t.removeAttr("href"),t.attr("data-toggle","dropdown"))});
 //# sourceMappingURL=maps/navigation-bar.js.map
