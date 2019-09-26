071af222ffc1cf5b0f03ec904a9187c9667f0c01
Oscar Keur <o.e.keur@gmail.com>
2019-09-26 16:28:34 -0400

    Include webp images as default in footer

--- functions.php
+++ functions.php
@@ -87,6 +87,16 @@ function change_title_placeholders() {
 
 add_filter( 'enter_title_here', 'change_title_placeholders' );
 
+function webp_upload_mimes( $existing_mimes ) {
+	// add webp to the list of mime types
+	$existing_mimes['webp'] = 'image/webp';
+
+	// return the array back to the function with our added mime type
+	return $existing_mimes;
+}
+add_filter( 'mime_types', 'webp_upload_mimes' );
+
+
 /**
  * Instantiate the GPNL settings menu.
  */
