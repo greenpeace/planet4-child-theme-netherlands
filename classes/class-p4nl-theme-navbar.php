ddbe78b55a49e4047b3b98f9cd633db86dd2e6c5
Oscar Keur <o.e.keur@gmail.com>
2019-10-09 14:54:57 +0200

    Refactor donation menu into own class

---
+++ classes/class-p4nl-theme-navbar.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Navbar customization
+ *
+ * @package P4NL_CT
+ */
+use Timber\Menu as TimberMenu;
+if ( ! class_exists( 'P4NL_Theme_Navbar' ) ) {
+	/**
+	 * Class P4NL_Theme_Navbar
+	 */
+
+	class P4NL_Theme_Navbar {
+
+		/**
+		 * Constructor
+		 */
+		public function __construct() {
+			$this->hooks();
+		}
+
+		/**
+		 * Initiate our hooks
+		 */
+		public function hooks() {
+			register_nav_menu( 'donation-menu', __( 'P4NL Donation Menu', 'planet4-master-theme-backend' ) );
+			$this->add_to_timbercontext( 'donation_navbar_menu', new TimberMenu( 'donation_menu' ) );
+		}
+
+		/**
+		 * Add new items to the general Timber context
+		 *
+		 * @param mixed $key The key with which to store data.
+		 * @param mixed $val The data to be stored.
+		 */
+		private function add_to_timbercontext( $key, $val ) {
+			add_filter(
+				'timber_context',
+				function ( $context ) {
+					global $timber_context;
+					$timber_context = is_array( $timber_context ) ? $timber_context : array();
+					$context        = is_array( $context ) ? $context : array();
+					$context        = array_merge( $timber_context, $context );
+					return $context;
+				}
+			);
+
+			global $timber_context;
+			$timber_context[ $key ] = $val;
+		}
+	}
+}
--- functions.php
+++ functions.php
@@ -27,36 +27,6 @@ function enqueue_editor_styles() {
 
 add_action( 'enqueue_block_editor_assets', 'enqueue_editor_styles' );
 
-// ----------------------------------------------------------------------------------------
-
-// register a new wpmenu for donation dropdown
-function p4nl_donation_menu() {
-	register_nav_menu( 'donation-menu', __( 'P4NL Donation Menu' ) );
-}
-add_action( 'init', 'p4nl_donation_menu' );
-
-// filter for adding stuff to the timber context
-add_filter(
-	'timber_context',
-	function ( $context ) {
-		global $timber_context;
-		$timber_context = is_array( $timber_context ) ? $timber_context : array();
-		$context        = is_array( $context ) ? $context : array();
-		$context        = array_merge( $timber_context, $context );
-		return $context;
-	}
-);
-
-// helperfunction to actually add stuff to the timber context
-function add_to_timbercontext( $key, $val ) {
-	global $timber_context;
-	$timber_context[ $key ] = $val;
-}
-// add the P4NL donation menu to to the timbercontact
-add_to_timbercontext( 'donation_navbar_menu', new TimberMenu( 'donation_menu' ) );
-
-// ----------------------------------------------------------------------------------------
-
 // Remove the ability for editors to add custom css
 function remove_custom_css_from_customizer() {
 	global $wp_customize;
