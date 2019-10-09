776f8f914da07ca870cb4309edf2f0cb7096fb52
Oscar Keur <o.e.keur@gmail.com>
2019-10-09 12:09:33 +0200

    Remove webp images from prod

--- functions.php
+++ functions.php
@@ -95,7 +95,7 @@ function webp_upload_mimes( $existing_mimes ) {
 	// return the array back to the function with our added mime type
 	return $existing_mimes;
 }
-add_filter( 'mime_types', 'webp_upload_mimes' );
+//add_filter( 'mime_types', 'webp_upload_mimes' );
 
 
 /**
