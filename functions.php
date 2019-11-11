d623370ede0c969fe78a960bda701ea714cc493c
Oscar Keur <o.e.keur@gmail.com>
2019-11-11 14:05:20 +0100

    Enable gutenberg templates

--- functions.php
+++ functions.php
@@ -64,16 +64,16 @@ add_filter( 'enter_title_here', 'change_title_placeholders' );
 // **
 // * Default template for pages.
 // */
-// function set_page_template() {
-//
-// $post_type_object = get_post_type_object( 'page' );
-// $post_type_object->template = array(
-// array( 'planet4-gpnl-blocks/hero-image'),
-// array( 'core/paragraph' ),
-// array( 'planet4-gpnl-blocks/newsletter' ),
-// );
-// }
-// add_action( 'init', 'set_page_template' );
+ function set_page_template() {
+
+ $post_type_object = get_post_type_object( 'page' );
+ $post_type_object->template = array(
+ array( 'planet4-gpnl-blocks/hero-image'),
+ array( 'core/paragraph' ),
+ array( 'planet4-gpnl-blocks/newsletter' ),
+ );
+ }
+ add_action( 'init', 'set_page_template' );
 
 /**
  * Hides login fields on everwhere except for dev environment.
