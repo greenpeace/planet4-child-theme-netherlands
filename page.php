5d7fe9793b965be90d81b0cac326bafb23eb4a14
Dirk Faber <d.j.faber@outlook.com>
2021-01-19 13:26:35 +0100

    feat: add comment possibility to a page.

---
+++ page.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * To generate specific templates for your pages you can use:
+ * /mytheme/views/page-mypage.twig
+ * (which will still route through this PHP file)
+ * OR
+ * /mytheme/page-mypage.php
+ * (in which case you'll want to duplicate this file and save to the above path)
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+/**
+ * Category : Issue
+ * Tag      : Campaign
+ * Post     : Action
+ */
+
+use P4\MasterTheme\Context;
+use P4\MasterTheme\Post;
+use Timber\Timber;
+
+
+$context        = Timber::get_context();
+$post           = new Post(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+$page_meta_data = get_post_meta( $post->ID );
+$page_meta_data = array_map( 'reset', $page_meta_data );
+
+// Set Navigation Issues links.
+$post->set_issues_links();
+
+// Get Navigation Campaigns links.
+$page_tags = wp_get_post_tags( $post->ID );
+$tags      = [];
+
+if ( is_array( $page_tags ) && $page_tags ) {
+	foreach ( $page_tags as $page_tag ) {
+		$tags[] = [
+			'name' => $page_tag->name,
+			'link' => get_tag_link( $page_tag ),
+		];
+	}
+	$context['campaigns'] = $tags;
+}
+
+// Set GTM Data Layer values.
+$post->set_data_layer();
+$data_layer = $post->get_data_layer();
+
+Context::set_header( $context, $page_meta_data, $post->title );
+Context::set_background_image( $context );
+Context::set_og_meta_fields( $context, $post );
+Context::set_campaign_datalayer( $context, $page_meta_data );
+
+$context['post']                = $post;
+$context['social_accounts']     = $post->get_social_accounts( $context['footer_social_menu'] );
+$context['page_category']       = $data_layer['page_category'];
+$context['post_tags']           = implode( ', ', $post->tags() );
+$context['custom_body_classes'] = 'brown-bg ';
+
+
+// Build an arguments array to customize WordPress comment form.
+$comments_args = [
+	'comment_notes_before' => '',
+	'comment_notes_after'  => '',
+	'comment_field'        => Timber::compile( 'comment_form/comment_field.twig' ),
+	'submit_button'        => Timber::compile( 'comment_form/submit_button.twig' ),
+	'title_reply'          => __( 'Leave your reply', 'planet4-master-theme' ),
+	'fields'               => apply_filters(
+		'comment_form_default_fields',
+		[
+			'author' => Timber::compile( 'comment_form/author_field.twig' ),
+			'email'  => Timber::compile( 'comment_form/email_field.twig' ),
+		]
+	),
+];
+
+$context['comments_args']       = $comments_args;
+$context['show_comments']       = comments_open( $post->ID );
+$context['post_comments_count'] = get_comments(
+	[
+		'post_id' => $post->ID,
+		'status'  => 'approve',
+		'type'    => 'comment',
+		'count'   => true,
+	]
+);
+
+if ( post_password_required( $post->ID ) ) {
+
+	// Hide the page title from links to the extra feeds.
+	remove_action( 'wp_head', 'feed_links_extra', 3 );
+
+	$context['login_url'] = wp_login_url();
+
+	Timber::render( 'single-page.twig', $context );
+} else {
+	Timber::render( [ 'page-' . $post->post_name . '.twig', 'page.twig' ], $context );
+}
