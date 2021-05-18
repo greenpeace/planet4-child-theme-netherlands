430e8ecb7c483ee1272976722c98bbf9cc3bd616
Oscar Keur <o.e.keur@gmail.com>
2021-05-18 17:18:32 +0200

    WIP: remove unneeded rules

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -18,19 +18,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env'],
-              ['@babel/preset-react'],
-            ]
-          }
-        }
-      },
-      {
         test: /\.s?css$/,
         use: [
           'style-loader',
