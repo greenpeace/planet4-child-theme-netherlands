b43d7ec15da53d2c1dfee783e2ef5cf209a20e1d
Oscar Keur <o.e.keur@gmail.com>
2021-08-23 13:36:26 +0200

    chore: add build files

--- public/build/bootstrap.asset.php
+++ public/build/bootstrap.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< develop
 <?php return array('dependencies' => array(), 'version' => 'c0ae39ab7bdf66b1abeb8b560836b0cc');
-=======
-<?php return array('dependencies' => array(), 'version' => '22a20ff58820a266dea9485a3e6a70f9');
->>>>>>> chore: add build files
--- public/build/child-theme-main.asset.php
+++ public/build/child-theme-main.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< develop
-<?php return array('dependencies' => array(), 'version' => 'c22531e7e49c11222bdd7f77b5046151');
-=======
-<?php return array('dependencies' => array(), 'version' => '187516a5dbe608ac74a5712fb2333fe9');
->>>>>>> chore: add build files
+<?php return array('dependencies' => array(), 'version' => '9d999febf469db66c90a4024b505119c');
