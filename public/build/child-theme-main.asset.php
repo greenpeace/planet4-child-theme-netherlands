49bfe148ddc3946a82788c013c332e61bf6e0fdf
Pablo Cuadrado <pablocuadrado@gmail.com>
2022-02-11 11:07:25 -0300

    style(ui): Footer styling.

--- public/build/child-theme-main.asset.php
+++ public/build/child-theme-main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '8873bb26669ef3d0c1e4f56f66214ccc');
+<?php return array('dependencies' => array(), 'version' => '29fba1fa4aaf262c10080ad11a448b46');
