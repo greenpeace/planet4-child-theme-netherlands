a4bb825da1b725d4e72f3adcd1f19d8eacb1081a
Claudia <ckok@greenpeace.org>
2025-04-24 16:19:39 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,3 +11,11 @@ function enqueue_child_styles() {
 
   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
+
+ function register_custom_script() {
+     if (is_front_page()) {
+         wp_enqueue_script('update-DOM-on-scroll', get_stylesheet_directory_uri() .  '/js/update-DOM-on-scroll.js');
+     }
+ }
+ 
+ add_action('wp_enqueue_scripts', 'register_custom_script');
