9ca4d8a5b62e0e1aaea88844a9c359c4ad5cddc6
Claudia <ckok@greenpeace.org>
2025-04-24 15:58:23 +0200

    Add files via upload

---
+++ js/update-DOM-on-scroll.js
@@ -0,0 +1,44 @@
+let isScrolled = false;
+
+ready(function () {
+  if (!onHomePage()) return;
+  if (getYPosition() > 0) {
+    isScrolled = true;
+    document.body.classList.add("scrolled");
+  }
+  window.addEventListener("scroll", trackScrollAsBodyClass);
+});
+
+function ready(fn) {
+  if (document.readyState !== "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+function onHomePage() {
+  return document.body.classList.contains("home");
+}
+
+function getYPosition() {
+  return (
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0
+  );
+}
+
+function trackScrollAsBodyClass() {
+  const yTransitionPoint = 1; // in case people don't scroll ALL the way up
+  const yPosition = getYPosition();
+
+  if (yPosition > yTransitionPoint && !isScrolled) {
+    isScrolled = true;
+    document.body.classList.add("scrolled");
+  } else if (yPosition === 0 && isScrolled) {
+    isScrolled = false;
+    document.body.classList.remove("scrolled");
+  }
+}
